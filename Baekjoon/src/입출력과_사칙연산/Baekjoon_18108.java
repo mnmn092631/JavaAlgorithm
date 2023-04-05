7fea992c3cfaaee2a8d14a85c70bfa2691d6fdde
juhee kang <mnmn092631@gmail.com>
2023-04-06 00:20:38 +0900

    18108번

---
+++ Baekjoon/src/입출력과_사칙연산/Baekjoon_18108.java
@@ -0,0 +1,15 @@
+package 입출력과_사칙연산;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Baekjoon_18108 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
+		int year = Integer.parseInt(br.readLine());		
+		System.out.print(year - 543);
+
+	}
+
+}
