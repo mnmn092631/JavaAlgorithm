ff7d57f008012a81c1d66261499cddb430678c6a
juhee kang <mnmn092631@gmail.com>
2023-04-06 00:46:48 +0900

    11382번

---
+++ Baekjoon/src/입출력과_사칙연산/Baekjoon_11382.java
@@ -0,0 +1,20 @@
+package 입출력과_사칙연산;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon_11382 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long a = Long.parseLong(st.nextToken());
+		long b = Long.parseLong(st.nextToken());
+		long c = Long.parseLong(st.nextToken());
+		
+		System.out.print(a + b + c);
+	}
+
+}
