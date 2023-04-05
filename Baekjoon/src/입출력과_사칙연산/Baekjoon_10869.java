086daab35ddefb648848020f06d1dbeed1c3984d
juhee kang <mnmn092631@gmail.com>
2023-04-06 00:03:52 +0900

    10869번

---
+++ Baekjoon/src/입출력과_사칙연산/Baekjoon_10869.java
@@ -0,0 +1,23 @@
+package 입출력과_사칙연산;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon_10869 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+		System.out.println(a % b);
+	}
+
+}
