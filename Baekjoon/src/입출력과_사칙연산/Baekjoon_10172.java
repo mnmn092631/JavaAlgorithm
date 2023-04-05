785844d9d907297fa0f25f97ebefc4a38e2b0001
juhee kang <mnmn092631@gmail.com>
2023-04-06 00:54:06 +0900

    10172번

---
+++ Baekjoon/src/입출력과_사칙연산/Baekjoon_10172.java
@@ -0,0 +1,13 @@
+package 입출력과_사칙연산;
+
+public class Baekjoon_10172 {
+
+	public static void main(String[] args) {
+		System.out.print("|\\_/|\n"
+						+ "|q p|   /}\n"
+						+ "( 0 )\"\"\"\\\n"
+						+ "|\"^\"`    |\n"
+						+ "||_/=\\\\__|");
+	}
+
+}
