eb87a34e35a0c6757276dc05f75980fab0bd1c08
juhee kang <mnmn092631@gmail.com>
2023-04-08 23:11:28 +0900

    11021번

---
+++ Baekjoon/src/반복문/Baekjoon_11021.java
@@ -0,0 +1,27 @@
+package 반복문;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon_11021 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < t; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			sb.append("Case #").append(i + 1).append(": ").append(a + b).append("\n");
+		}
+		
+		System.out.print(sb);
+	}
+
+}
