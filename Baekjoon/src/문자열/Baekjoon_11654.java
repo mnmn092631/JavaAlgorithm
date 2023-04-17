6ba7dcf1d769d7d336818f66e5ffc2de2e1e6388
juhee kang <mnmn092631@gmail.com>
2023-04-17 11:45:02 +0900

    11654번

---
+++ Baekjoon/src/문자열/Baekjoon_11654.java
@@ -0,0 +1,17 @@
+package 문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon_11654 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int c = br.read();
+
+		System.out.print(c);
+	}
+
+}
