6d71e3889761b78d53eee6026d04adb689dd3a7c
juhee kang <mnmn092631@gmail.com>
2023-04-17 11:21:01 +0900

    2743번

---
+++ Baekjoon/src/문자열/Baekjoon_2743.java
@@ -0,0 +1,16 @@
+package 문자열;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Baekjoon_2743 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		
+		System.out.print(str.length());
+	}
+
+}
