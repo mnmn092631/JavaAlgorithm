92ff341c8eb7d01427b08e161c63c01629d4ce8b
juhee kang <mnmn092631@gmail.com>
2023-04-27 09:40:31 +0900

    27323번

---
+++ Baekjoon/src/기하_직사각형과_삼각형/Baekjoon_27323.java
@@ -0,0 +1,18 @@
+package 기하_직사각형과_삼각형;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon_27323 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+		
+		System.out.print(a * b);
+	}
+
+}
