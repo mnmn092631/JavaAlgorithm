7e5d6ab54d5e570bafaedd85b8401c87ccc13883
juhee kang <mnmn092631@gmail.com>
2023-05-02 15:14:30 +0900

    11651번

---
+++ Baekjoon/src/정렬/Baekjoon_11651.java
@@ -0,0 +1,34 @@
+package 정렬;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Baekjoon_11651 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+
+		int n = Integer.parseInt(br.readLine());
+		int[][] arr = new int[n][2];
+
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr, (a, b) -> a[1] == b[1] ? a[0] - b[0] : a[1] - b[1]);
+
+		for (int[] coor : arr) {
+			sb.append(coor[0]).append(" ").append(coor[1]).append("\n");
+		}
+
+		System.out.print(sb);
+	}
+
+}
