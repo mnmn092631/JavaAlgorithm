44f2cddd8f5af62dbe55535ec2c15d63fd19c3cd
juhee kang <mnmn092631@gmail.com>
2023-05-03 14:21:34 +0900

    14425번

---
+++ Baekjoon/src/집합과_맵/Baekjoon_14425.java
@@ -0,0 +1,31 @@
+package 집합과_맵;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Baekjoon_14425 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		HashSet<String> set = new HashSet<>();
+		int count = 0;
+
+		for (int i = 0; i < n; i++) {
+			set.add(br.readLine());
+		}
+
+		for (int i = 0; i < m; i++) {
+			if (set.contains(br.readLine())) count++;
+		}
+
+		System.out.print(count);
+	}
+
+}
