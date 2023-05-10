ea5c1a69bafae7f46ca5e4b98ba3d70465887241
juhee kang <mnmn092631@gmail.com>
2023-05-10 16:44:54 +0900

    15439번

---
+++ Baekjoon/src/조합론/Baekjoon_15439.java
@@ -0,0 +1,15 @@
+package 조합론;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon_15439 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		System.out.print(n * (n - 1));
+	}
+
+}
