8bdd53cc6a803a8da60dae9549bfa3eced490ccd
juhee kang <mnmn092631@gmail.com>
2023-05-31 17:13:18 +0900

    5338번

---
+++ Baekjoon/src/Baekjoon_5338.java
@@ -0,0 +1,11 @@
+public class Baekjoon_5338 {
+
+	public static void main(String[] args) {
+		System.out.print("       _.-;;-._\n"
+						+ "'-..-'|   ||   |\n"
+						+ "'-..-'|_.-;;-._|\n"
+						+ "'-..-'|   ||   |\n"
+						+ "'-..-'|_.-''-._|");
+	}
+
+}
