ce7a4b876a528e83bee266bc7fa544c8448bba85
juhee kang <mnmn092631@gmail.com>
2023-05-31 17:16:37 +0900

    4101번

---
+++ Baekjoon/src/Baekjoon_4101.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon_4101 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+
+		while (true) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			if (a + b == 0) break;
+			
+			if (a > b) sb.append("Yes").append("\n");
+			else sb.append("No").append("\n");
+		}
+		
+		System.out.print(sb);
+	}
+
+}
