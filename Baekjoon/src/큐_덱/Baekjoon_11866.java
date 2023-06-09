b824c110364ba0225f259af0d8d3708fd0854a34
juhee kang <mnmn092631@gmail.com>
2023-06-09 16:03:14 +0900

    11866번

--- Baekjoon/src/큐_덱/Baekjoon_11866.java
+++ Baekjoon/src/큐_덱/Baekjoon_11866.java
@@ -3,6 +3,8 @@ package 큐_덱;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Baekjoon_11866 {
@@ -16,28 +18,18 @@ public class Baekjoon_11866 {
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
 		
-		boolean[] check = new boolean[n];
-		int idx = 0;
-		int count = 0;
-		int delCount = 0;
-		while (delCount < n) {
-			while(check[idx]) {
-				idx = (idx + 1) % n;
-			}
-			if (count != k - 1) {
-				count++;
-				idx = (idx + 1) % n;
-				continue;
-			}
-			sb.append(idx + 1).append(", ");
-			check[idx] = true;
-			idx = (idx + 1) % n;
-			delCount++;
-			count = 0;
+		List<Integer> list = new ArrayList<>();
+		for (int i = 1; i <=n; i++) {
+			list.add(i);
 		}
 
-		sb.delete(sb.length() - 2, sb.length() + 1);
-		sb.append(">");
+		int idx = k - 1;
+		while (n > 1) {
+			sb.append(list.remove(idx)).append(", ");
+			idx = (idx + k - 1) % --n;
+		}
+
+		sb.append(list.get(0)).append(">");
 		System.out.print(sb);
 	}
 
