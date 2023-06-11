f489da052acfec54376652909f7285f81ad698f3
juhee kang <mnmn092631@gmail.com>
2023-06-11 20:18:11 +0900

    6월 3주 - 정렬(7983번)

---
+++ AlgorithmStudy/src/june_03/Baekjoon_7983.java
@@ -0,0 +1,36 @@
+package june_03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Baekjoon_7983 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int n = Integer.parseInt(br.readLine());
+
+		int[][] arr = new int[n][2];
+
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr, (a, b) -> b[1] - a[1]);
+		
+		int result = arr[0][1];
+		for(int[] a : arr) {
+			if(result > a[1]) result = a[1];
+			result -= a[0];
+		}
+		
+		System.out.print(result);
+	}
+
+}
