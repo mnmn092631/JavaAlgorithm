3b20206d3121eb6f1d43743076564928ce353958
juhee kang <mnmn092631@gmail.com>
2023-06-20 10:35:59 +0900

    11399번

---
+++ Baekjoon/src/Baekjoon_11399.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Baekjoon_11399 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int n = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr);
+		long total = 0;
+		long sec = 0;
+
+		for (int num : arr) {
+			sec += num;
+			total += sec;
+		}
+
+		System.out.print(total);
+	}
+
+}
