2e4b46a497a862784b3b5fd6682c0e6c1024eebe
juhee kang <mnmn092631@gmail.com>
2023-06-23 12:27:53 +0900

    1927번

---
+++ Baekjoon/src/Baekjoon_1927.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Baekjoon_1927 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int n = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> que = new PriorityQueue<>();
+
+		while (n-- > 0) {
+			int num = Integer.parseInt(br.readLine());
+			if (num == 0) {
+				if (que.isEmpty()) sb.append("0\n");
+				else sb.append(que.remove()).append("\n");
+			}
+			else que.offer(num);
+		}
+
+		System.out.print(sb);
+	}
+
+}
